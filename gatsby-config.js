f93bef758ba082394f7b528d69843c414ae59cd4
Swizec Teller <swizec@swizec.com>
2019-10-21 23:15:13 -0700

    Add oembeds, I think

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,48 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.mdx`, `.md`],
+                gatsbyRemarkPlugins: [{
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 10000,
+                            linkImagesToOriginal: false,
+                            quality: 80,
+                            withWebp: true,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-copy-linked-files`
+                    },
+                    {
+                        resolve: `gatsby-remark-numbered-footnotes`
+                    },
+                    {
+                        resolve: `gatsby-remark-smartypants`
+                    },
+                    {
+                        resolve: 'gatsby-remark-external-links',
+                        options: {
+                            target: '_blank',
+                            rel: 'noreferrer', // eslint-disable-line unicorn/prevent-abbreviations
+                        },
+                    },
+                    {
+                        resolve: '@raae/gatsby-remark-oembed',
+                        options: {
+                            usePrefix: false,
+                            providers: {
+                                exclude: ['Reddit']
+                            }
+                        }
+                    }
+                ],
+                remarkPlugins: [require(`remark-slug`)], // eslint-disable-line global-require
+            },
+        },
+        {
             resolve: `gatsby-plugin-manifest`,
             options: {
                 name: `Swizec Teller`,
